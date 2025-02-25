f8bc6a1ba99ef15dd3482f63055afe9f4a3caa04
rohira-dhruv <dhruvjrohira@gmail.com>
2025-02-25 13:48:59 +0530

    implement DELETE API

--- src/main/java/com/learnings/cashcard/CashCardController.java
+++ src/main/java/com/learnings/cashcard/CashCardController.java
@@ -5,13 +5,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -69,6 +64,15 @@ public class CashCardController {
     CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
     cashCardRepository.save(updatedCashCard);
     return ResponseEntity.noContent().build();
+  }
+
+  @DeleteMapping("/{requestedId}")
+  public ResponseEntity<Void> deleteCashCard(@PathVariable Long requestedId, Principal principal) {
+    if (cashCardRepository.existsByIdAndOwner(requestedId, principal.getName())) {
+      cashCardRepository.deleteById(requestedId);
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.notFound().build();
   }
 
   private CashCard findCashCard(Long requestedId, Principal principal) {
